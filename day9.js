27fb4765b4d30b05ebb0cdda8cb3a991f3b8da8b
Aryan Gajjar <102947440+gajjararyan@users.noreply.github.com>
2024-02-29 13:45:54 +0530

    Create day9.js

---
+++ day9.js
@@ -0,0 +1,15 @@
+/* Which of the following are examples of node modules?
+
+a. Express
+
+b. Body-parser
+
+c. Socket.io
+
+d. All of the above
+
+/*
+
+Answer D) Allof the Above 
+
+Explanation -> Express , bobdy parser  and many more things like socket.io  are all examples of Node.js modules.
