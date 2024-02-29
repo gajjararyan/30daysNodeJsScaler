cdaec6ceb547bbfaaf9134300e78774ff9139897
Aryan Gajjar <102947440+gajjararyan@users.noreply.github.com>
2024-02-29 14:03:05 +0530

    Create day23.js

---
+++ day23.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  // other product fields
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
